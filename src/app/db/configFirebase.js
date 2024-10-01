400d00599bfdf0aacaa19c5326c245db444e698a
Jbtzcodes <laminutedecode@gmail.com>
2024-10-01 11:42:31 +0200

    Update

--- src/app/db/configFirebase.js
+++ src/app/db/configFirebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBT2YkX1GWYNYaarw2e4sbs4iKSulVO2w0",
-  authDomain: "next-auth-tuto-28120.firebaseapp.com",
-  projectId: "next-auth-tuto-28120",
-  storageBucket: "next-auth-tuto-28120.appspot.com",
-  messagingSenderId: "229092750382",
-  appId: "1:229092750382:web:913f9b5a48407c5bbe1bbf"
+  
 };
 
 
